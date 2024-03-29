587f7b33fd82f918e0b2a656d55891fb0c8ce338
charles7668 <charles76680119@gmail.com>
2024-03-29 23:58:34 +0800

    [Refactor] Format code

--- LoaderDll/LoaderDll.cpp
+++ LoaderDll/LoaderDll.cpp
@@ -167,52 +167,63 @@ LeCreateProcess(
 	LOOP_ONCE
 	{
 		if (Leb == nullptr)
-		break;
+			break;
 
-	ULONG_PTR   ExtraSize;
-	PVOID       MaximumAddress;
-	PREGISTRY_REDIRECTION_ENTRY64 Entry64;
+		ULONG_PTR ExtraSize;
+		PVOID MaximumAddress;
+		PREGISTRY_REDIRECTION_ENTRY64 Entry64;
 
-	ExtraSize = Leb->NumberOfRegistryRedirectionEntries * sizeof(Leb->RegistryReplacement[0]);
+		ExtraSize = Leb->NumberOfRegistryRedirectionEntries * sizeof(Leb->RegistryReplacement[0]);
 
-	if (ExtraSize != 0)
-	{
-		MaximumAddress = Leb->RegistryReplacement + Leb->NumberOfRegistryRedirectionEntries;
-		FOR_EACH(Entry64, Leb->RegistryReplacement, Leb->NumberOfRegistryRedirectionEntries)
+		if (ExtraSize != 0)
 		{
-			MaximumAddress = ML_MAX(MaximumAddress, PtrAdd(PtrAdd(Entry64->Original.SubKey.Buffer,      Leb),   Entry64->Original.SubKey.MaximumLength));
-			MaximumAddress = ML_MAX(MaximumAddress, PtrAdd(PtrAdd(Entry64->Original.ValueName.Buffer,   Leb),   Entry64->Original.ValueName.MaximumLength));
-			MaximumAddress = ML_MAX(MaximumAddress, PtrAdd(PtrAdd((PVOID)Entry64->Original.Data,        Leb),   Entry64->Original.DataSize));
+			MaximumAddress = Leb->RegistryReplacement + Leb->NumberOfRegistryRedirectionEntries;
+			FOR_EACH(Entry64, Leb->RegistryReplacement, Leb->NumberOfRegistryRedirectionEntries)
+			{
+				MaximumAddress = ML_MAX(MaximumAddress,
+				                        PtrAdd(PtrAdd(Entry64->Original.SubKey.Buffer, Leb), Entry64->Original.SubKey.
+					                        MaximumLength));
+				MaximumAddress = ML_MAX(MaximumAddress,
+				                        PtrAdd(PtrAdd(Entry64->Original.ValueName.Buffer, Leb), Entry64->Original.
+					                        ValueName.MaximumLength));
+				MaximumAddress = ML_MAX(MaximumAddress,
+				                        PtrAdd(PtrAdd((PVOID)Entry64->Original.Data, Leb), Entry64->Original.DataSize));
 
-			MaximumAddress = ML_MAX(MaximumAddress, PtrAdd(PtrAdd(Entry64->Redirected.SubKey.Buffer,    Leb),   Entry64->Redirected.SubKey.MaximumLength));
-			MaximumAddress = ML_MAX(MaximumAddress, PtrAdd(PtrAdd(Entry64->Redirected.ValueName.Buffer, Leb),   Entry64->Redirected.ValueName.MaximumLength));
-			MaximumAddress = ML_MAX(MaximumAddress, PtrAdd(PtrAdd((PVOID)Entry64->Redirected.Data,      Leb),   Entry64->Redirected.DataSize));
+				MaximumAddress = ML_MAX(MaximumAddress,
+				                        PtrAdd(PtrAdd(Entry64->Redirected.SubKey.Buffer, Leb), Entry64->Redirected.
+					                        SubKey.MaximumLength));
+				MaximumAddress = ML_MAX(MaximumAddress,
+				                        PtrAdd(PtrAdd(Entry64->Redirected.ValueName.Buffer, Leb), Entry64->Redirected.
+					                        ValueName.MaximumLength));
+				MaximumAddress = ML_MAX(MaximumAddress,
+				                        PtrAdd(PtrAdd((PVOID)Entry64->Redirected.Data, Leb), Entry64->Redirected.
+					                        DataSize));
+			}
+
+			ExtraSize += PtrOffset(MaximumAddress, Leb);
 		}
 
-		ExtraSize += PtrOffset(MaximumAddress, Leb);
-	}
+		LePeb = OpenOrCreateLePeb((ULONG_PTR)CurrentTeb()->ClientId.UniqueProcess, TRUE, ExtraSize);
+		if (LePeb == nullptr)
+		{
+			Status = STATUS_NONE_MAPPED;
+			break;
+		}
 
-	LePeb = OpenOrCreateLePeb((ULONG_PTR)CurrentTeb()->ClientId.UniqueProcess, TRUE, ExtraSize);
-	if (LePeb == nullptr)
-	{
-		Status = STATUS_NONE_MAPPED;
-		break;
-	}
+		CopyMemory(&LePeb->Leb, Leb, FIELD_OFFSET(LEB, NumberOfRegistryRedirectionEntries) + ExtraSize);
 
-	CopyMemory(&LePeb->Leb, Leb, FIELD_OFFSET(LEB, NumberOfRegistryRedirectionEntries) + ExtraSize);
+		LePeb->LdrLoadDllAddress = LookupExportTable(GetNtdllHandle(), NTDLL_LdrLoadDll);
+		LePeb->LdrLoadDllBackupSize = LDR_LOAD_DLL_BACKUP_SIZE;
+		CopyMemory(LePeb->LdrLoadDllBackup, LePeb->LdrLoadDllAddress, LDR_LOAD_DLL_BACKUP_SIZE);
 
-	LePeb->LdrLoadDllAddress = LookupExportTable(GetNtdllHandle(), NTDLL_LdrLoadDll);
-	LePeb->LdrLoadDllBackupSize = LDR_LOAD_DLL_BACKUP_SIZE;
-	CopyMemory(LePeb->LdrLoadDllBackup, LePeb->LdrLoadDllAddress, LDR_LOAD_DLL_BACKUP_SIZE);
+		ULONG_PTR Length = (StrLengthW(DllFullPath) + 1) * sizeof(WCHAR);
+		CopyMemory(LePeb->LeDllFullPath, DllFullPath, ML_MIN(Length, sizeof(LePeb->LeDllFullPath)));
 
-	ULONG_PTR Length = (StrLengthW(DllFullPath) + 1) * sizeof(WCHAR);
-	CopyMemory(LePeb->LeDllFullPath, DllFullPath, ML_MIN(Length, sizeof(LePeb->LeDllFullPath)));
+		Length = Module->FullDllName.Length + sizeof(WCHAR) - Module->BaseDllName.Length;
 
-  Length = Module->FullDllName.Length + sizeof(WCHAR) - Module->BaseDllName.Length;
-
-  Length = ML_MIN(Length, sizeof(LePeb->LeDllFullPath));
-  CopyMemory(LePeb->LeDllDirPath, Module->FullDllName.Buffer, Length);
-  LePeb->LeDllDirPath[Length / sizeof(WCHAR) - 1] = 0;
+		Length = ML_MIN(Length, sizeof(LePeb->LeDllFullPath));
+		CopyMemory(LePeb->LeDllDirPath, Module->FullDllName.Buffer, Length);
+		LePeb->LeDllDirPath[Length / sizeof(WCHAR) - 1] = 0;
 	}
 
 	UNICODE_STRING DllFullPathString;
